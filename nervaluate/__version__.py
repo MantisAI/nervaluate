25d25016621cb5e65b788d015363ccd6c923f046
Matthew Upson <matthew.a.upson@gmail.com>
2019-12-06 18:41:52 -0300

    chg: Update package url to point at pypi

--- nervaluate/__version__.py
+++ nervaluate/__version__.py
@@ -1,7 +1,7 @@
 __name__="nervaluate"
 __version__="0.1.4"
 __description__="NER evaluation done right"
-__url__="https://www.github.com/ivyleavedtoadflax/nervaluate"
+__url__="https://pypi.org/project/nervaluate"
 __author__="David Batista, Matthew Upson"
 __author_email__="david.batista@gmail.com,matthew.a.upson@gmail.com"
 __license__="GNU GPL v3"
