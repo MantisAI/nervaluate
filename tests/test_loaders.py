8864dc168dd299caffef2930ca2ff6c70ac13447
David S. Batista <dsbatista@gmail.com>
2025-05-20 20:54:56 +0200

    fixing loading test_list_loader

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -62,21 +62,6 @@ def test_loaders_produce_the_same_results():
 
 
 @pytest.fixture
-def conll_data():
-    return """PER\t0\t0
-ORG\t2\t3
-LOC\t5\t5
-
-PER\t0\t0
-ORG\t2\t2"""
-
-
-@pytest.fixture
-def list_data():
-    return [["PER", "O", "ORG", "O", "LOC"], ["PER", "O", "ORG"]]
-
-
-@pytest.fixture
 def dict_data():
     return [
         [
@@ -88,34 +73,114 @@ def dict_data():
     ]
 
 
-def test_conll_loader(conll_data):
+def test_conll_loader():
     """Test CoNLL format loader."""
+    true_conll = (
+        "word\tO\nword\tO\nword\tO\nword\tO\nword\tO\nword\tO\n\n"
+        "word\tO\nword\tO\nword\tB-ORG\nword\tI-ORG\nword\tO\nword\tO\n\n"
+        "word\tO\nword\tO\nword\tB-MISC\nword\tI-MISC\nword\tO\nword\tO\n\n"
+        "word\tB-MISC\nword\tI-MISC\nword\tI-MISC\nword\tI-MISC\nword\tI-MISC\nword\tI-MISC\n"
+    )
+
+    pred_conll = (
+        "word\tO\nword\tO\nword\tB-PER\nword\tI-PER\nword\tO\nword\tO\n\n"
+        "word\tO\nword\tO\nword\tB-ORG\nword\tI-ORG\nword\tO\nword\tO\n\n"
+        "word\tO\nword\tO\nword\tB-MISC\nword\tI-MISC\nword\tO\nword\tO\n\n"
+        "word\tB-MISC\nword\tI-MISC\nword\tI-MISC\nword\tI-MISC\nword\tI-MISC\nword\tI-MISC\n"
+    )
+
     loader = ConllLoader()
-    entities = loader.load(conll_data)
+    true_entities = loader.load(true_conll)
+    pred_entities = loader.load(pred_conll)
 
-    assert len(entities) == 2  # Two documents
-    assert len(entities[0]) == 3  # First document has 3 entities
-    assert len(entities[1]) == 2  # Second document has 2 entities
+    # Test true entities
+    assert len(true_entities) == 4  # Four documents
+    assert len(true_entities[0]) == 0  # First document has no entities (all O tags)
+    assert len(true_entities[1]) == 1  # Second document has 1 entity (ORG)
+    assert len(true_entities[2]) == 1  # Third document has 1 entity (MISC)
+    assert len(true_entities[3]) == 1  # Fourth document has 1 entity (MISC)
 
-    # Check first entity
-    assert entities[0][0].label == "PER"
-    assert entities[0][0].start == 0
-    assert entities[0][0].end == 0
+    # Check first entity in second document
+    assert true_entities[1][0].label == "ORG"
+    assert true_entities[1][0].start == 2
+    assert true_entities[1][0].end == 3
+
+    # Test pred entities
+    assert len(pred_entities) == 4  # Four documents
+    assert len(pred_entities[0]) == 1  # First document has 1 entity (PER)
+    assert len(pred_entities[1]) == 1  # Second document has 1 entity (ORG)
+    assert len(pred_entities[2]) == 1  # Third document has 1 entity (MISC)
+    assert len(pred_entities[3]) == 1  # Fourth document has 1 entity (MISC)
+
+    # Check first entity in first document
+    assert pred_entities[0][0].label == "PER"
+    assert pred_entities[0][0].start == 2
+    assert pred_entities[0][0].end == 3
+
+    # Test empty document handling
+    empty_doc = "word\tO\nword\tO\nword\tO\n\n"
+    empty_entities = loader.load(empty_doc)
+    assert len(empty_entities) == 1  # One document
+    assert len(empty_entities[0]) == 0  # Empty list for document with only O tags
 
 
-def test_list_loader(list_data):
+def test_list_loader():
     """Test list format loader."""
+    true_list = [
+        ["O", "O", "O", "O", "O", "O"],
+        ["O", "O", "B-ORG", "I-ORG", "O", "O"],
+        ["O", "O", "B-MISC", "I-MISC", "O", "O"],
+        ["B-MISC", "I-MISC", "I-MISC", "I-MISC", "I-MISC", "I-MISC"],
+    ]
+
+    pred_list = [
+        ["O", "O", "B-PER", "I-PER", "O", "O"],
+        ["O", "O", "B-ORG", "I-ORG", "O", "O"],
+        ["O", "O", "B-MISC", "I-MISC", "O", "O"],
+        ["B-MISC", "I-MISC", "I-MISC", "I-MISC", "I-MISC", "I-MISC"],
+    ]
+
     loader = ListLoader()
-    entities = loader.load(list_data)
+    true_entities = loader.load(true_list)
+    pred_entities = loader.load(pred_list)
 
-    assert len(entities) == 2  # Two documents
-    assert len(entities[0]) == 3  # First document has 3 entities
-    assert len(entities[1]) == 2  # Second document has 2 entities
+    # Test true entities
+    assert len(true_entities) == 4  # Four documents
+    assert len(true_entities[0]) == 0  # First document has no entities (all O tags)
+    assert len(true_entities[1]) == 1  # Second document has 1 entity (ORG)
+    assert len(true_entities[2]) == 1  # Third document has 1 entity (MISC)
+    assert len(true_entities[3]) == 1  # Fourth document has 1 entity (MISC)
 
-    # Check first entity
-    assert entities[0][0].label == "PER"
-    assert entities[0][0].start == 0
-    assert entities[0][0].end == 0
+    # Check no entities in the first document
+    assert len(true_entities[0]) == 0
+
+    # Check first entity in second document
+    assert true_entities[1][0].label == "ORG"
+    assert true_entities[1][0].start == 2
+    assert true_entities[1][0].end == 3
+
+    # Check only entity in the last document
+    assert true_entities[3][0].label == "MISC"
+    assert true_entities[3][0].start == 0
+    assert true_entities[3][0].end == 5
+
+    # Test pred entities
+    assert len(pred_entities) == 4  # Four documents
+    assert len(pred_entities[0]) == 1  # First document has 1 entity (PER)
+    assert len(pred_entities[1]) == 1  # Second document has 1 entity (ORG)
+    assert len(pred_entities[2]) == 1  # Third document has 1 entity (MISC)
+    assert len(pred_entities[3]) == 1  # Fourth document has 1 entity (MISC)
+
+    # Check first entity in first document
+    assert pred_entities[0][0].label == "PER"
+    assert pred_entities[0][0].start == 2
+    assert pred_entities[0][0].end == 3
+
+    # Test empty document handling
+    empty_doc = [["O", "O", "O"]]
+    empty_entities = loader.load(empty_doc)
+    assert len(empty_entities) == 1  # One document
+    assert len(empty_entities[0]) == 0  # Empty list for document with only O tags
 
 
 def test_dict_loader(dict_data):
